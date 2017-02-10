d103201fda3aa5bc336a749053b79504e9478d4a
Stefan Nowak <stekenno@gmail.com>
2017-02-10 15:44:46 +0000

    Add basic sbUserLogin component

--- client-web-app/app/app.jsx
+++ client-web-app/app/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SampleComponent from './components/sampleComponent';
 import UsersList from './components/userList';
+import SBUserLogin from './components/messaging/sbUserLogin';
 
 ReactDOM.render(
   <div className="view">
     <SampleComponent />
     <UsersList />
+    <SBUserLogin />
   </div>
   , document.querySelector('.app'),
 );
