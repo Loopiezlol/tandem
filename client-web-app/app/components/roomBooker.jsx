af17d668ab43b69127b77f1471654558a02ce360
Adrian Delgado <adrian.delgado-vlaic@kcl.ac.uk>
2017-02-12 04:21:54 +0000

    add RoomBooker method

---
+++ client-web-app/app/components/roomBooker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RoomBooker() {
+  return (
+    <iframe src="https://www.smoothbook.co/ap/index.php/smart/home/8009#/service" />
+  );
+}
+
+export default RoomBooker;
