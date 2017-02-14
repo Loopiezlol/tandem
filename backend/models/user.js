e9167576668a95005d2594c7854e8c1631dec303
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-02-14 21:57:15 +0000

    user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId=Schema.ObjectId;
 
 const User = new mongoose.Schema({
-	id: ObjectId,
-	firstName:String,
-	lastName:String,
-	email : {type:String, unique: true },
-	password: String,
+  username: { type: String, required: true, unique: true },
+  firstName: String,
+  lastName: String,
+  email: { type: String, unique: true },
+  password: String,
+  temp: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('User', User);
