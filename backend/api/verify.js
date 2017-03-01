d74322a2c498b7cc4978bd41030cc1435c5bb585
viktor <viktor@viktor>
2017-03-01 04:47:27 +0000

    Fixed registration, added confirmations emails (sends 2 atm)

---
+++ backend/api/verify.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const nev = require('email-verification')(mongoose);
+const bodyParser = require('body-parser');
+const User = require('../models/user');
+const TempUser = require('../models/tempUser');
+
+myHasher = function(password, tempUserData, insertTempUser, callback) {
+  bcrypt.genSalt(8, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      return insertTempUser(hash, tempUserData, callback);
+    });
+  });
+};
+
+nev.configure({
+  persistentUserModel: User,
+  tempUserModel: TempUser,
+  expirationTime: 600, // 10 minutes
+
+  verificationURL: 'http://localhost:3000/email-verification/${URL}',
+  transportOptions: {
+    service: 'Gmail',
+    auth: {
+      user: 'daybreak.vk@gmail.com',
+      pass: 'daybreak1',                       //ADD E-MAIL USERNAMER HERE
+    }
+  },
+
+  hashingFunction: myHasher,
+  passwordFieldName: 'password',
+}, function(err, options) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('configured: ' + (typeof options === 'object'));
+});
+
+function verifyUrl (req, res) {
+  var url = req.params.URL;
+  const user =  TempUser.findOne({GENERATED_VERIFYING_URL: url}, function(err, document) {
+     const user = document;
+  });
+
+  nev.confirmTempUser(url,function(err, user) {
+    if (user) {
+      nev.sendConfirmationEmail(user.email, function(err, info) {
+        if (err) {
+          return res.status(404).send('ERROR: sending confirmation email FAILED');
+        }
+      res.redirect('/'); //if all goes well, go to login page (TODO: display message on top "Successfully verified!")
+      });
+    } else {
+      return res.status(404).send('ERROR: confirming temp user FAILED');
+    }
+  });
+  };
+
+
+router.get('/email-verification/:URL', verifyUrl);
+
+module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -17,35 +17,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/email-verification/:URL', function(req, res) {
-  var url = req.params.URL;
-
-//Const user = find the user with this token in database!
-  TempUser.findOne({GENERATED_VERIFYING_URL: url}, function(err, document) {
-  const user = document;
-});
-
-// MAKE _ WAIT FOR ^ TO FINISH!!!
-
-  nev.confirmTempUser(url, function(err, user) {
-    if (user) {
-      nev.sendConfirmationEmail(user.email, function(err, info) {
-        if (err) {
-          return res.status(404).send('ERROR: sending confirmation email FAILED');
-        }
-      res.redirect('/'); //if all goes well, go to login page (TODO: display message on top "Successfully verified!")
-      });
-    } else {
-      return res.status(404).send('ERROR: confirming temp user FAILED');
-    }
-  });
-});
-
-
 
 app.use('/users/', require('./api/users'));
 app.use('/', require('./api/login'));
 app.use('/', require('./api/register'));
+app.use('/', require('./api/verify'));
 
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
