f4dde6c49955f9147b5e926a8af34506a1d6a776
Stefan Nowak <stekenno@gmail.com>
2017-03-09 16:41:17 +0000

    comment out messaging for now.

--- client-web-app/app/app.jsx
+++ client-web-app/app/app.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RegisterComponent from './components/registerComponent';
 import LoginComponent from './components/loginComponent';
 import './styles/example.scss';
 import './styles/example2.scss';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import SBMessaging from './components/messaging/sbMessaging';
 // import SampleComponent from './components/sampleComponent';
 // import UsersList from './components/userList';
-// import SBMessaging from './components/messaging/sbMessaging';
-
-// ReactDOM.render(
-//   <div className="view">
-//     {/* }<SampleComponent />
-//   <UsersList /> */}
-//     <MuiThemeProvider>
-//       <SBMessaging />
-//     </MuiThemeProvider>
-//   </div>, document.querySelector('.app'),
-// );
 
 ReactDOM.render(
+    // <MuiThemeProvider>
+    //   <SBMessaging />
+    // </MuiThemeProvider>
   <Router history={browserHistory}>
     <Route path="/" component={LoginComponent} />
     <Route path="/login" component={LoginComponent} />
