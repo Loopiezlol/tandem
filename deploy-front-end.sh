431b16bd0ee3c0dfb1898b967df10d024effbc5a
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-16 14:10:37 +0100

    rm dist

--- deploy-front-end.sh
+++ deploy-front-end.sh
@@ -5,12 +5,12 @@ then
     echo "deploying web-app"
     cd client-web-app/dist/
     git init
-    git commit --allow-empty -m "deply web-app"
     git checkout -b gh-pages
     touch .nojekyll
     git add .
-    git commit -am "deply web-app"
+    git commit -m "deply web-app"
     git push https://github.kcl.ac.uk/k1502800/Tandem.git gh-pages --force
+
 else
     echo "Need clean working directory to publish"
 fi
