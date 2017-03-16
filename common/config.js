ee390138f6dbae1dbe908e36c700d725e790a7c5
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-16 16:34:27 +0100

    update mail password

--- common/config.js
+++ common/config.js
@@ -2,6 +2,6 @@ module.exports = {
   secret: 'toblazeornottoblazethatisthequestion',
   db: process.env.NODE_ENV === 'production' ? 'mongodb://heroku_rpncztz0:k9iiuej7kikgaobmqld0g2faf9@ds133290.mlab.com:33290/heroku_rpncztz0' : 'mongodb://localhost/tandem',
   mailerLogin: 'daybreak.vk@gmail.com',
-  mailerPassword: 'daybreak1',
+  mailerPassword: 'theyfoundmypassword',
   server: process.env.NODE_ENV === 'production' ? 'https://tandem-kcl.herokuapp.com' : 'http://localhost:3000',
 };
