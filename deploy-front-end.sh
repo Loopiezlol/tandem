2c901cab418b18fad2ab0ea618d9c54e3831e4aa
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-16 12:25:07 +0100

    got it

--- deploy-front-end.sh
+++ deploy-front-end.sh
@@ -2,12 +2,14 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i "" 'client-web-app/dist/*' ./.gitignore
-    git add .
-    git commit -m "Edit .gitignore to publish"
-    git subtree push --prefix client-web-app/dist origin gh-pages --force
-    git reset HEAD~
-    git checkout .gitignore
+    # sed -i "" 'client-web-app/dist/*' ./.gitignore
+    # git add .
+    # git commit -m "Edit .gitignore to publish"
+    git subtree split --prefix client-web-app/dist -b gh-pages
+    git push -f origin gh-pages:gh-pages
+    git branch -D gh-pages
+    # git reset HEAD~
+    # git checkout .gitignore
 else
     echo "Need clean working directory to publish"
 fi
