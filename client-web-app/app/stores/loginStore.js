aa11554e93732955d7464f7df7128efbb26f49a3
Stefan Nowak <stekenno@gmail.com>
2017-03-21 16:28:18 +0000

    Fix import error which causes webpack failure to compile

--- client-web-app/app/stores/loginStore.js
+++ client-web-app/app/stores/loginStore.js
@@ -1,6 +1,6 @@
 import Reflux from 'reflux';
 import request from 'superagent';
-import actions from '../actions';
+import actions from '../actions/actions';
 import SBActions from '../actions/sbActions';
 
 class LoginStore extends Reflux.Store {
