af57fa057dbc33a0f8e4b90784d79eed4a8b0d9e
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-27 16:44:24 +0100

    comment some stuff

--- backend/app.js
+++ backend/app.js
@@ -57,12 +57,12 @@ if (require.main === module) {
   mongoose.connect(config.db);
   // require('./helpers/usergenerator').populateDBwithLanguages();
 
-  require('./helpers/usergenerator').populateDB(50);
+  // require('./helpers/usergenerator').populateDB(50);
   // UNCOMMENT THIS TO GENERATE SOME LANGUAGES AND LEVELS
-  const wrap = require('co-express');
+  // const wrap = require('co-express');
 
-  wrap(createLanguages)();
-  wrap(createLevels)();
+  // wrap(createLanguages)();
+  // wrap(createLevels)();
 } else {
   mongoose.connect('mongodb://localhost/test');
 }
