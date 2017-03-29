a508d7a634743d0e947e7310c759ef043cf7a765
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-29 23:54:28 +0100

    another cors try

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,11 @@ const generators = require('./helpers/db-data-generator');
 
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* Another port to run
 the app on 3000 localhost
