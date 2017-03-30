c70caef4d811a3f8dfd921332389a1f579814848
Catalin Buruiana <catalin.buruiana@kcl.ac.uk>
2017-03-30 17:45:36 +0100

    change email config pls google

--- common/config.js
+++ common/config.js
@@ -2,8 +2,8 @@
 module.exports = {
   secret: 'toblazeornottoblazethatisthequestion',
   db: process.env.NODE_ENV === 'production' ? 'mongodb://heroku_6jpxlz5h:52lakerms0car7kcmg6au1b8ek@ds131510.mlab.com:31510/heroku_6jpxlz5h' : 'mongodb://localhost/tandem',
-  mailerLogin: 'kcltandem@gmail.com',
-  mailerPassword: 'pleasechange',
+  mailerLogin: 'tandemkcl@gmail.com',
+  mailerPassword: 'pleasechangeme',
   server: process.env.NODE_ENV === 'production' ? 'https://tandem-kcl.herokuapp.com' : 'http://localhost:3000',
   client: process.env.NODE_ENV === 'production' ? 'https://github.kcl.ac.uk/pages/k1502800/Tandem' : 'http://localhost:3001',
 };
